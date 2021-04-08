02e423e99a9bf06b69cb748364bd47e274d5058d
Christopher Murphy <flyweight@pm.me>
2021-04-08 15:17:27 -0600

    refactor: console table data, mod tidy

--- dryer/console.go
+++ dryer/console.go
@@ -8,19 +8,24 @@ import (
 )
 
 func Print(clones [][]Token) {
-	tableData := make([][]string, len(clones))
-	srcBeg := clones[0][0]
-	srcEnd := clones[0][1]
-	patBeg := clones[1][0]
-	patEnd := clones[1][1]
-	tableData = append(tableData,
-		[]string{srcBeg.Filename, fmt.Sprintf("%d:%d", srcBeg.Line, srcBeg.Column), fmt.Sprintf("%d:%d", srcEnd.Line, srcEnd.Column)},
-		[]string{patBeg.Filename, fmt.Sprintf("%d:%d", patBeg.Line, patBeg.Column), fmt.Sprintf("%d:%d", patEnd.Line, patEnd.Column)},
-	)
+	data := cloneTableData(clones)
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Filepath", "Start", "End"})
 	table.SetBorder(false)
 	table.SetAutoFormatHeaders(false)
-	table.AppendBulk(tableData)
-	table.Render() // Send output
+	table.AppendBulk(data)
+	table.Render()
+}
+
+func cloneTableData(clones [][]Token) [][]string {
+	td := make([][]string, len(clones))
+	srcBeg := clones[0][0]
+	srcEnd := clones[0][1]
+	patBeg := clones[1][0]
+	patEnd := clones[1][1]
+	td = append(td,
+		[]string{srcBeg.Filename, fmt.Sprintf("%d:%d", srcBeg.Line, srcBeg.Column), fmt.Sprintf("%d:%d", srcEnd.Line, srcEnd.Column)},
+		[]string{patBeg.Filename, fmt.Sprintf("%d:%d", patBeg.Line, patBeg.Column), fmt.Sprintf("%d:%d", patEnd.Line, patEnd.Column)},
+	)
+	return td
 }
